5d0fdf3dabf6b1ae63ce6f6a2bae5e1e3f087bbe
lost-melody <lost__melody@163.com>
2026-05-08 01:34:11 +0800

    feat: displays app icon list in dock view

--- src/widgets/dock_view.rs
+++ src/widgets/dock_view.rs
@@ -1,4 +1,5 @@
 use gtk::glib;
+use gtk::prelude::*;
 use gtk::subclass::prelude::*;
 use gtk4 as gtk;
 
@@ -22,20 +23,31 @@ impl DockView {
     pub fn destroy(&self) {
         self.imp().destroy();
     }
+
+    /// Unsets the prelight state flag if not hovered.
+    ///
+    /// This is used to fix it when the prelight flag is not properly unset on popover menu closed.
+    pub fn refresh_prelight(&self) {
+        if !self.motion().unwrap().contains_pointer() {
+            self.unset_state_flags(gtk::StateFlags::PRELIGHT);
+        }
+    }
 }
 
 mod imp {
     use std::cell::RefCell;
 
     use declarative::{block, construct};
-    use gtk::glib;
     use gtk::prelude::*;
     use gtk::subclass::prelude::*;
+    use gtk::{gio, glib};
     use gtk4 as gtk;
 
+    use crate::models;
     use crate::prelude::*;
     use crate::services::niri;
     use crate::utils::log;
+    use crate::widgets;
 
     type Obj = super::DockView;
 
@@ -43,12 +55,19 @@ mod imp {
     #[properties(wrapper_type = Obj)]
     pub struct DockViewImpl {
         timer: RefCell<Option<glib::SourceId>>,
-
-        revealer: RefCell<Option<gtk::Revealer>>,
+        niri: RefCell<Option<niri::Niri>>,
         peek: RefCell<Option<gtk::Revealer>>,
 
-        niri: RefCell<Option<niri::Niri>>,
-
+        /// The connector of the window's output monitor, e.g. `DP-1`.
+        #[property(get, set)]
+        output: RefCell<String>,
+        /// Filtered application information list to display.
+        #[property(get, set)]
+        apps: RefCell<Option<gtk::FilterListModel>>,
+        #[property(get)]
+        motion: RefCell<Option<gtk::EventControllerMotion>>,
+        #[property(get)]
+        revealer: RefCell<Option<gtk::Revealer>>,
         #[property(get)]
         view: RefCell<Option<gtk::CenterBox>>,
     }
@@ -58,6 +77,10 @@ mod imp {
 
         fn on_constructed(&self) {
             let obj = self.obj();
+            self.apps.replace(Some(gtk::FilterListModel::new(
+                None::<gio::ListModel>,
+                None::<gtk::CustomFilter>,
+            )));
 
             block!(obj.clone() {
                 set_orientation: gtk::Orientation::Vertical
@@ -81,13 +104,16 @@ mod imp {
                             ~
 
                             append: &_ @gtk::Button {
+                                tooltip_text: "App Launcher"
+
                                 child: &_ @gtk::Image {
                                     icon_name: "applications-all-symbolic"
                                     icon_size: gtk::IconSize::Large
                                 }
                                 ~
 
-                                add_css_class: "neodock-launcher-button"
+                                add_css_class: "neodock-icon-button"
+                                add_css_class: "neodock-icon-button-launcher"
                                 connect_clicked: |_| {
                                     if let Err(err) = std::process::Command::new("qs").
                                         args(["-c", "noctalia-shell", "ipc", "call", "launcher", "toggle"]).
@@ -120,28 +146,47 @@ mod imp {
                 }
             });
 
+            let motion = gtk::EventControllerMotion::new();
+            self.obj().add_controller(motion.clone());
+
+            self.motion.replace(Some(motion));
             self.revealer.replace(Some(revealer));
             self.peek.replace(Some(peek));
             self.view.replace(Some(view));
 
+            let model = obj.apps().unwrap();
+            flow_box.bind_model(Some(&model), |o| {
+                let app_info = o.downcast_ref::<models::App>().unwrap();
+                let app_icon = widgets::AppIcon::new();
+                app_icon.set_app_info(Some(app_info));
+                app_icon.upcast()
+            });
+
             self.bind_application();
+            self.bind_root_window();
             self.connect_state_flags();
         }
 
         /// Finds the [crate::NeoDockApp] and retrieves the [niri::Niri] object.
         fn bind_application(&self) {
-            self.obj()
-                .with_application(|obj, app| match app.downcast::<crate::NeoDockApp>() {
-                    Ok(app) => {
-                        let niri = app.niri().clone();
-                        obj.imp().connect_niri_overview(&niri);
-                        obj.imp().niri.replace(Some(niri));
-                    }
-                    Err(app) => {
-                        log::warning!("a NeoDockApp is required");
-                        app.quit();
-                    }
+            self.obj().with_application(|obj, app| {
+                let app = app.downcast::<crate::NeoDockApp>().expect("NeoDockApp required");
+                obj.apps().unwrap().set_model(app.sorted_apps().as_ref());
+                let niri = app.niri().clone();
+                obj.imp().connect_niri_overview(&niri);
+                obj.imp().niri.replace(Some(niri));
+            });
+        }
+
+        /// Finds the [crate::NeoWindow] and retrieves the output connector.
+        fn bind_root_window(&self) {
+            self.obj().with_root_window(|_, win| {
+                let win = win.downcast::<crate::NeoWindow>().expect("NeoWindow required");
+                win.connect_output_notify(|win| {
+                    let output = win.output();
+                    log::message!("window output: {output}");
                 });
+            });
         }
 
         /// Detects dock hovered events.
