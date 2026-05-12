f485ac9bdbd2b229e0fb014efd765f697a8d2510
lost-melody <lost__melody@163.com>
2026-05-12 22:31:47 +0800

    feat: added object signals manager

--- src/utils/signal.rs
+++ src/utils/signal.rs
@@ -5,6 +5,57 @@ use gtk::glib;
 use gtk::prelude::*;
 use gtk4 as gtk;
 
+/// A signals store that manages object's event signals,
+/// and disconnects them at once on dropped.
+pub struct Signals<T: IsA<glib::Object>> {
+    object: glib::WeakRef<T>,
+    signals: Vec<Option<glib::SignalHandlerId>>,
+}
+
+impl<T: IsA<glib::Object>> Drop for Signals<T> {
+    fn drop(&mut self) {
+        self.disconnect_all();
+    }
+}
+
+impl<T: IsA<glib::Object>> Signals<T> {
+    pub fn new(object: &T) -> Self {
+        Self {
+            object: object.downgrade(),
+            signals: Vec::default(),
+        }
+    }
+
+    pub fn object(&self) -> Option<T> {
+        self.object.upgrade()
+    }
+
+    /// Adds signal handler id to managed list.
+    pub fn add_signal(&mut self, signal: glib::SignalHandlerId) {
+        self.signals.push(Some(signal));
+    }
+
+    pub fn disconnect_all(&mut self) {
+        if let Some(object) = self.object() {
+            for signal in &mut self.signals {
+                object.disconnect(signal.take().unwrap());
+            }
+        }
+        self.signals = Vec::default();
+    }
+}
+
+pub trait AssignSignalsExt<T: IsA<glib::Object>> {
+    /// Assigns signal handler id to [Signals].
+    fn assign_signals(self, signals: &mut Signals<T>);
+}
+
+impl<T: IsA<glib::Object>> AssignSignalsExt<T> for glib::SignalHandlerId {
+    fn assign_signals(self, signals: &mut Signals<T>) {
+        signals.add_signal(self);
+    }
+}
+
 /// Helper used to implement "connect_once".
 pub struct OnceCallback<F>(Rc<RefCell<(Option<F>, Option<glib::SignalHandlerId>)>>);
 
